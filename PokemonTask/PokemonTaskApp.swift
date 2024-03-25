5bf4831f96c95a4405985efd13e10e39a88c68ca
Ilya Sudnik <i.sudnik@rubylabs.com>
2024-03-25 11:41:23 +0100

    Initial Commit

---
+++ PokemonTask/PokemonTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonTaskApp.swift
+//  PokemonTask
+//
+//  Created by Ilya Sudnik on 25.03.24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
